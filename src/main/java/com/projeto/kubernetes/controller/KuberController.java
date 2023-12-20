0e0376c70341ffba3421fd304f4b036d7480e289
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-12-20 17:29:28 +0000

    organizando pasta

--- src/main/java/com/projeto/kubernetes/controller/KuberController.java
+++ src/main/java/com/projeto/kubernetes/controller/KuberController.java
@@ -1,4 +1,4 @@
-package com.projeto.kubernetes;
+package com.projeto.kubernetes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
