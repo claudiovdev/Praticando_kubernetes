f2a71d2df9d37648b4faa02e7adf5c4dac9b7a45
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-12-21 15:17:41 +0000

    atualizando serviço de consultar ip

--- src/main/java/com/projeto/kubernetes/controller/KuberController.java
+++ src/main/java/com/projeto/kubernetes/controller/KuberController.java
@@ -3,12 +3,8 @@ package com.projeto.kubernetes.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("meu-ip")
@@ -17,16 +13,15 @@ public class KuberController {
     @GetMapping
     public String retornandoIp() {
         try {
-            return obterEnderecoIPPublico();
-        } catch (IOException | RuntimeException e) {
-            throw new RuntimeException("Erro ao obter o endereço IP", e);
+            return obterEnderecoIPInstancia();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Erro ao obter o endereço IP da instância", e);
         }
     }
 
-    private static String obterEnderecoIPPublico() throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL("http://checkip.amazonaws.com").openStream()))) {
-            return "meu ip atual é: " + br.readLine().trim();
-        }
+    private static String obterEnderecoIPInstancia() throws UnknownHostException {
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        return "O endereço IP da instância é: " + ip;
     }
 
 }
