59131614f55da1d7b4f4c27162bd3e922a549a20
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-12-22 06:33:03 +0000

    atualizando

--- src/main/java/com/projeto/kubernetes/controller/KuberController.java
+++ src/main/java/com/projeto/kubernetes/controller/KuberController.java
@@ -34,7 +34,7 @@ public class KuberController {
     public ResponseEntity<String> lerArquivo() {
         try {
 
-            String caminhoArquivo = "arquivo/mensagem.txt";
+            String caminhoArquivo = "/arquivo/mensagem.txt";
 
             // Carrega o arquivo da pasta resources
             Resource resource = new ClassPathResource(caminhoArquivo);
