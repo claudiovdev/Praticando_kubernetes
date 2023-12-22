68eae3381f13057a75d29eeff031ca52d04cc523
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-12-22 06:20:08 +0000

    atualizando arquivo kubernetes para ter um bolume

--- src/main/java/com/projeto/kubernetes/controller/KuberController.java
+++ src/main/java/com/projeto/kubernetes/controller/KuberController.java
@@ -1,16 +1,22 @@
 package com.projeto.kubernetes.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
-@RequestMapping("meu-ip")
+@RequestMapping("kubernet")
 public class KuberController {
 
-    @GetMapping
+    @GetMapping("/meu-ip")
     public String retornandoIp() {
         try {
             return obterEnderecoIPInstancia();
@@ -23,5 +29,28 @@ public class KuberController {
         String ip = InetAddress.getLocalHost().getHostAddress();
         return "O endereço IP da instância é: " + ip;
     }
+
+    @GetMapping("/ler")
+    public ResponseEntity<String> lerArquivo() {
+        try {
+
+            String caminhoArquivo = "arquivo/mensagem.txt";
+
+            // Carrega o arquivo da pasta resources
+            Resource resource = new ClassPathResource(caminhoArquivo);
+            Path path = Path.of(resource.getURI());
+
+            // Lê o conteúdo do arquivo
+            String conteudo = Files.readString(path);
+
+            // Retorna o conteúdo como resposta
+            return ResponseEntity.ok(conteudo);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("Erro ao ler o arquivo.");
+        }
+    }
+
+
 
 }
